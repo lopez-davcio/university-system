00844db0df734a7acaeaaaf90bf662f1e1c62a18
Lopez Davcio <lopez.davcio@gmail.com>
2025-08-10 18:01:18 +0200

    add recalculate_gpa() and show_courses_and_grades() on Grades

--- grades.py
+++ grades.py
@@ -1,10 +1,25 @@
 
 class Grades:
 
-    def __init__(self, student_id, grades={}, gpa=None):
+    def __init__(self, student_id, gpa=None):
         self._student_id = student_id
-        self._grades = grades
+        self._grades = {}
         self._gpa = gpa
         
+        
+    def recalculate_gpa(self):
+        """Recalculate the GPA by averaging all grades in _grades, and update the _gpa attribute."""
+        grade_sum = 0
+        grade_count = len(self._grades)
+        for key, value in self._grades:
+            grade_sum += value
+        new_gpa = grade_sum / grade_count
+        self._gpa = new_gpa
+        
+
+    def show_courses_and_grades(self):
+        for key, value in self._grades:
+            print(f"Course code: {key}, grade: {value}")
+
 
     
